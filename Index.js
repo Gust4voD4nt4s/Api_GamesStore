b2fc106a9c0ef6c3c71bfb489b51b6ccc01d7c25
gust4vod4nt4s <gustavo1234henrique.gd@gmail.com>
2022-12-10 14:28:36 -0300

    Chamando rotas

--- Index.js
+++ Index.js
@@ -2,6 +2,8 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const db = require('./src/utils/db')
+const videoGameRouter = require('./src/routes/videoGame.route')
+const gameRouter = require('./src/routes/game.route')
 const port = 3000
 
 app.use(bodyParser.json())
@@ -9,6 +11,9 @@ app.use(bodyParser.json())
 app.get("/", (res) => {
     res.send('api_game_store is running')
 })
+
+app.use('/api/v1/videoGame', videoGameRouter)
+app.use('/api/v1/game', gameRouter)
 
 app.use((error, res) => {
     console.log('Error', error)
--- src/routes/videoGame.route.js
+++ src/routes/videoGame.route.js
@@ -1,4 +1,4 @@
-const router = require('express').Router
+const router = require('express').Router()
 const controller = require('../controllers/videoGame.controller')
 
 router.post('/', controller.save)
