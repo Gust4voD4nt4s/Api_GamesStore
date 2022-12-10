7b5a1d8fd6b14dc344bd5fbd3c29233944896d72
gust4vod4nt4s <gustavo1234henrique.gd@gmail.com>
2022-12-10 14:02:22 -0300

    Criando schema do video game

---
+++ src/models/videoGame.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const videoGameSchema = mongoose.Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+
+    model: {
+        type: String,
+        required: true
+    },
+
+    cpu: {
+        type: String,
+        required: true
+    },
+
+    gpu: {
+        type: String,
+        required: true
+    },
+
+    storageSize: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Video Game', videoGameSchema)
