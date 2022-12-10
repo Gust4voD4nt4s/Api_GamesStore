a6d1dd1b759012c58cd3a2ec1cf55c2dcc3def4b
gust4vod4nt4s <gustavo1234henrique.gd@gmail.com>
2022-12-10 14:17:27 -0300

    Criado rotas do crud de video game

---
+++ src/routes/videoGame.route.js
@@ -0,0 +1,14 @@
+const router = require('express').Router
+const controller = require('../controllers/videoGame.controller')
+
+router.post('/', controller.save)
+
+router.get('/', controller.getAll)
+
+router.get('/:id', controller.getById)
+
+router.put('/:id', controller.update)
+
+router.delete('/:id', controller.remove)
+
+module.exports = router
